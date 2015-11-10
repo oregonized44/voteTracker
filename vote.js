acc41c6fdbbd0f9498e00c8c8f09f8d1b46c81ca
Jacob Porter Herold <dcejake@gmail.com>
2015-11-10 15:28:44 -0800

    Unfinished, but working on pushing array objects around. saving for postarity

--- vote.js
+++ vote.js
@@ -1,14 +1,4 @@
-
-
-
-
-
-
-
 //UTILITY FUNCTIONS AND ARRAYS ===============================================
-
-
-
 var tree1 = new imageConstructor("images/tree1.jpg");
 var tree2 = new imageConstructor("images/tree2.jpg");
 var tree3 = new imageConstructor("images/tree3.jpg");
@@ -18,80 +8,106 @@ var city3 = new imageConstructor("images/city3.jpg");
 
 treeDirectory = [tree1, tree2, tree3];
 cityDirectory = [city1, city2, city3];
+midwayTreeDirectory = [];
+midwayCityDirectory = [];
+emptyHatTrees = [];
+emptyHatCities = [];
+
 //GET A RANDOM TREE IMAGE
-
-var x;
-
 function randomTree() {            //selects our tree image
-
-  x = Math.floor(Math.random() * treeDirectory.length);
-
+ x = Math.floor(Math.random() * treeDirectory.length);
+ return x;
 };
 var randomTreePlaceholder = randomTree();
-//GET A RANDOM CITY IMAGE
-function randomCity(){             //selects our city image
 
-  var y = Math.floor(Math.random() * cityDirectory.length);
-  return y;
+
+//GET A RANDOM CITY IMAGE`
+function randomCity(){             //selects our city image
+ var y = Math.floor(Math.random() * cityDirectory.length);
+ return y;
 };
 var randomCityPlaceholder = randomCity();
-
-var emptyHatTrees = [];
-var emptyHatCities = [];
 
 leftImageEl = document.getElementById("leftImage");
 rightImageEl = document.getElementById("rightImage");
 resetButtonEl = document.getElementById("resetButton");
 voteButtonEl = document.getElementById("voteButton");
 
-/*leftImageEl.addEventListener("click", selectImage);
-rightImageEl.addEventListener("click", selectImage);
-leftImageEl.addEventListener("mouseover", selectImage);
-rightImageEl.addEventListener("mouseover", selectImage);*/
-
 
 //IMAGE DATA OBJECT CONSTRUCTION
 function imageConstructor(filePath){
-
 this.filePath = filePath;
 this.voteNumber = Math.floor((Math.random() * 49 ) + 2);     ///utility zone
 }
 
+
 //OnPageLoad =================================================================
+
 (function(){
-leftImageEl.src  = "images/tree3.jpg" ;
-console.log(treeDirectory[1].filePath);
-rightImageEl.src = "images/city2.jpg" ;
-console.log(cityDirectory[2].filePath);//(cityDirectory[x].filePath);
-//leftImageEl.innerHTML = "<img id='leftImage' src='" +  + "'";
-//rightImageEl.innerHTML = "<img id='rightimage' scr='" + cityDirectory[randomCityPlaceholder].filePath + "'"
-
-
-
-
+leftImageEl.src  = treeDirectory[randomTreePlaceholder].filePath;
+midwayTreeDirectory = slice(treeDirectory[randomTreePlaceholder]);
+rightImageEl.src = cityDirectory[randomCityPlaceholder].filePath;
+midwayCityDirectory = slice(cityDirectory[randomCityPlaceholder]);
 })();
 
-/*//PRINT IMAGES TO PAGE
-function upLoadImages(left,right){   //function will put images on page
-var left = leftImageEl ;             //  accesing our dom elemnt by Id
-var right= rightImageEl  ;           //  accessing our dom elenment by id
-leftImageEl = get random tree;
-rightImageEl = get random city;
-push left image on;
-push right image on;
+leftImageEl.addEventListener("click", selectImage);
+rightImageEl.addEventListener("click", selectImage);
+
+leftImageEl.addEventListener("mouseover", selectImage);
+rightImageEl.addEventListener("mouseover", selectImage);
+
+
+voteButton.addEventListener("click", voteSubmit);
+
+
+////////////////////select-image///////////////////////////////////////////////
+
+function selectImg() {
+this.className= "imgPicked";
+}
+
+
+function resetImages(){
+  treesDirectory  = emptyHatTrees;
+
+  citiesDirectory = emptyHatCities;
+
+}
+
+
+
+///////////////////////////////////////////////////////////////////
+
+/*
+function voteSubmit(){
+
+if (document.getElementsByClassName("imgPicked")){
+  voteNumber++;
+}
+
+emptyHatTrees.slice(midwayTreesDirectory[0]);
+emptyHatCities.slice(midwayCitiesDirectory[0]);
+
+
+
+if((citiesDirectory.length === 0) && (treesDirectory.length === 0)){
+resetImgs();
+
+}
+
+function resetPool() {
+resetImgs();
+
 
 
 }
-//MAKE CHART DYNAMICALLY BASED ON IMAGES
+//find images used and add to empty hat array
 
-//NEW PAIR FUNCTIONS TRIGGER FUNCITON
 
-function newPair(){
+//add chart to dom
 
-}
 
-//COUNT VOTES
-function recordVote(){
+//change photo to new source
 
 
 }
@@ -99,4 +115,15 @@ function recordVote(){
 
 
 
+
+/*var treeVoteTotal =  //Added functionality for later (cumulitive chart)
+
+
+for (ii = 0; ii<treeDirectory.length; ii++ ){
+
+
+total+=
+
+
+}
 
